fcfc5a05bfb2cd04f70787afb48482b4f142ff81
AyjayJr <ayjayjr@hotmail.com>
2023-04-14 18:12:42 -0400

    finished beta showcase area

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,9 +1,18 @@
-
+import image from '../assets/showcase.jpg'
 
 export default function Showcase() {
 	return(
-		<div className='showcase'>
+		<div
+			className='showcase'
+			style={{ backgroundImage:`url(${image})`}}
+		>
 			<div className='container'>
+				<div className='showcase-content'>
+					<h1>Good food choices are good investments.</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum amet leo.</p>
+					<button>Order Now</button>
+					<button>Learn More</button>
+				</div>
 			</div>
 		</div>
 	);
