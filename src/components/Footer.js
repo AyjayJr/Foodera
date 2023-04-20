1af7b0708583d99af1709585e4dfa0a30d74de3a
AyjayJr <ayjayjr@hotmail.com>
2023-04-20 17:28:13 -0400

    footer page links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,17 @@ export default function Footer() {
 	return (
 		<div className='footer-bg'>
 			<div className='container'>
+				<div className='links'>
+					<div className='pages'>
+						<a href='#'>Register</a>
+						<a href='#'>Forum</a>
+						<a href='#'>Affiliate</a>
+						<a href='#'>FAQ</a>
+					</div>
+					<div className='socials'>
 
+					</div>
+				</div>
 			</div>
 		</div>
 	);
