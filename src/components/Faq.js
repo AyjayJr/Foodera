a5596580dff45591a1f9d8a36c89e8d742082560
AyjayJr <ayjayjr@hotmail.com>
2023-04-24 15:32:55 -0400

    faq page beta done

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -5,10 +5,22 @@ export default function Faq() {
 				<h2>Frequently Asked Questions</h2>
 			</div>
 			<div className='faq-grid'>
-				<div>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.</div>
-				<div>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.</div>
-				<div>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.</div>
-				<div>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.</div>
+				<div>
+					<h3>Is Foodera Bread really baked fresh each day?</h3>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.</p>
+				</div>
+				<div>
+					<h3>Do you bake breads containing animal fats or products?</h3>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.</p>
+				</div>
+				<div>
+					<h3>Can I order your products online?</h3>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.</p>
+				</div>
+				<div>
+					<h3>When are you opening a shop near me?</h3>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.</p>
+				</div>
 			</div>
 		</div>
 	);
