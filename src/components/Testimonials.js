c53ea50e584043eb90772aaad1c204684ab82c1f
AyjayJr <ayjayjr@hotmail.com>
2023-05-05 10:53:51 -0400

    fixed testimonial index issue

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -2,12 +2,13 @@ import Carousel from "./Carousel";
 
 export default function Testimonials() {
 	return (
-		<Carousel>
-			<div className='testimonial-bg'>
-				<h2>Testimonials</h2>
-			</div>
-			<img src="https://via.placeholder.com/1600x300" alt="placeholder" />
-			<img src="https://via.placeholder.com/1600x300" alt="placeholder" />
-		</Carousel>
+		<div className='testimonial-bg'>
+			<h2>Testimonials</h2>
+				<Carousel>
+					<div className='testimonial'></div>
+					<img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+					<img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+				</Carousel>
+		</div>
 	);
 }
