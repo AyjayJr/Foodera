a3006dbc039d1c46e42316c199bffc0145a6db4a
AyjayJr <ayjayjr@hotmail.com>
2023-05-10 17:36:06 -0400

    burger menu addition, needs functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import logo from '../assets/logo.png';
+import menu from '../assets/bar.png';
 
 // TODO: media query for smaller viewport / sticky / transparent > white
 export default function Header() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const menuHandler = () => {
+		setIsMenuOpen((prevState) => { return !prevState })
+	}
+
 	return (
 		<header>
 			<div className='container'>
 				<nav>
-					<img src={logo} />
+					<img className='logo' src={logo} alt='logo' />
 					<ul>
 						<li>Home</li>
 						<li>About Us</li>
@@ -16,6 +23,7 @@ export default function Header() {
 						<li>FAQ</li>
 						<li><button>1-800-123-456</button></li>
 					</ul>
+					<img className='menu' src={menu} alt='menu icon' />
 				</nav>
 			</div>
 		</header>
