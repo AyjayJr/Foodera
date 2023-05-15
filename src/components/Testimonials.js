c6e83c8574fcb603e29937a039a01edaee91b3e2
AyjayJr <ayjayjr@hotmail.com>
2023-05-15 16:09:51 -0400

    testimonial section mostly done, TODO: make arrows visible

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,13 +1,28 @@
 import Carousel from "./Carousel";
+import pfp1 from "../assets/pfp1.jpg";
+import pfp2 from "../assets/pfp2.jpg";
+import pfp3 from "../assets/pfp3.jpg";
 
 export default function Testimonials() {
 	return (
 		<div className='testimonial-bg'>
 			<h2>Testimonials</h2>
 				<Carousel>
-					<div className='testimonial'></div>
-					<img src="https://via.placeholder.com/1600x300" alt="placeholder" />
-					<img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+					<div className='testimonial'>
+						<img className='pfp' src={pfp1} alt='Author'/>
+						<p className='quote'>"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live"</p>
+						<p className='author'>John Doe - Web Designer</p>
+					</div>
+					<div className='testimonial'>
+						<img className='pfp' src={pfp2} alt='Author'/>
+						<p className='quote'>"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live"</p>
+						<p className='author'>John Doe - Web Designer</p>
+					</div>
+					<div className='testimonial'>
+						<img className='pfp' src={pfp3} alt='Author'/>
+						<p className='quote'>"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live"</p>
+						<p className='author'>John Doe - Web Designer</p>
+					</div>
 				</Carousel>
 		</div>
 	);
