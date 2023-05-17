6d9b88f4f8042d8249df5b7cf7c1d86c04a04ac2
AyjayJr <ayjayjr@hotmail.com>
2023-05-17 16:06:29 -0400

    modal menu beta finished

--- src/components/ModalMenu.js
+++ src/components/ModalMenu.js
@@ -1,7 +1,14 @@
-export default function ModalMenu() {
+export default function ModalMenu(props) {
     return (
-        <div className='modal-bg'>
+        <div className='modal-bg' onClick={props.onClick}>
             <div className='modal-menu'>
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>About Us</a></li>
+                    <li><a href='#'>Explore Foods</a></li>
+                    <li><a href='#'>Reviews</a></li>
+                    <li><a href='#'>FAQ</a></li>
+                </ul>
             </div>
         </div>
     );
